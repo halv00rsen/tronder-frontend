4ad14b03fbc0918c4e9c4268c9bca4830965aa49
Jørgen Halvorsen <jorgen.aunet.halvorsen@gmail.com>
2019-10-22 10:36:43 +0200

    show user information

--- src/views/UserView/UserView.tsx
+++ src/views/UserView/UserView.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import { inject } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import { Auth } from 'aws-amplify';
 
 import './UserView.css';
+import { InjectedStoreProps } from 'store/Store';
+import { Redirect } from 'react-router';
+import { routes } from 'routes';
 
-const UserView: React.FC = () => {
+const UserView: React.FC<InjectedStoreProps> = (props) => {
 
   const logout = () => {
     Auth.signOut();
   };
 
+  if (!props.store.system.user) {
+    return <Redirect to={routes.home.path}/>;
+  }
   return (
     <div className="user-view">
+      <div className="user-view-name">
+        {props.store.system.user.name}
+      </div>
+      <div className="user-view-email">
+        {props.store.system.user.email}
+      </div>
       <button className="logout-button" onClick={logout}>Logg ut</button>
     </div>
   );
 };
 
-export default inject('store')(UserView);
+export default inject('store')(observer(UserView));
