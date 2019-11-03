6143ccf37135b27078584ca6e9945c0ea7de52e8
Jørgen Halvorsen <jorgen.aunet.halvorsen@gmail.com>
2019-11-03 20:37:30 +0100

    test-change for ci

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -7,7 +7,7 @@ import './HomeView.css';
 const HomeView: React.FC = () => {
   return (
     <div className="home-view">
-      <h2>Velkommen til Trøndr</h2>
+      <h2>Velkommen til Trøndr!</h2>
       <p>Trøndr er en nettside for alle ord og uttrykk for alle Norges dialekter</p>
       <NavLink to={routes.dialect.path}>
         For å sette i gang, trykk her
